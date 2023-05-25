95aba05a1fd3c2139cbbeca24129e9b74e22ad3e
CASPIAN76 <thakren76@gmail.com>
2023-05-25 08:48:42 -0700

    material modules

---
+++ src/app/shared-modules/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,  MatButtonModule,MatCardModule,MatGridListModule
+  ],
+  exports:[
+    MatButtonModule,MatCardModule,MatGridListModule
+  ]
+})
+export class MaterialModule { }
